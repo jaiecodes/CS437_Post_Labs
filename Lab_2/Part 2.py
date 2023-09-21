9b28ced54ba2934fbff754bf3bb3ef4a6b76ebdb
Sid <martianinja37@gmail.com>
2023-09-21 15:02:00 -0500

    update

--- Lab_2/Part 2.py
+++ Lab_2/Part 2.py
@@ -3,19 +3,25 @@ import datetime
 
 import numpy as np
 import time
+
+import os
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import CircularOutput
 from libcamera import controls
-
-
+from gtts import gTTS
+from sense_hat import SenseHat
+import socket
 import cv2
 picam2=Picamera2()  ## Create a camera object
 
 
-
+sense = SenseHat()
 dispW=1280
 dispH=720
+blue= (0,0,255)
+yellow= (255,255,0)
+red=(255,0,0)
 ## Next, we configure the preview window size that determines how big should the image be from the camera, the bigger the image the more the details you capture but the slower it runs
 ## the smaller the size, the faster it can run and get more frames per second but the resolution will be lower. We keep 
 picam2.preview_configuration.main.size= (dispW,dispH)  ## 1280 cols, 720 rows. Can also try smaller size of frame as (640,360) and the largest (1920,1080)
@@ -34,6 +40,14 @@ picam2.start()
 
 faceCascade=cv2.CascadeClassifier("/home/pi/Desktop/Code/Lab1/Lab1_Part2/haarcascade_frontalface_default.xml")
 
+def send_message():
+    sock.sendto(bytes(message, "utf-8"), (ip_addr, port_num))
+
+
+message = "MOTION DETECTED"
+ip_addr = "10.192.102.127"
+port_num = 5000
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
 while True:
     #tstart=time.time()
     frame=picam2.capture_array() ## frame is a large 2D array of rows and cols and at intersection of each point there is an array of three numbers for RGB i.e. [R,G,B] where RGB value ranges from 0 to 255
@@ -48,8 +62,10 @@ while True:
     key=cv2.waitKey(1) & 0xFF
     frameGray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces=faceCascade.detectMultiScale(frameGray,1.3,5)
+    if len(faces) > 0 :
+        send_message()
+        
     
-    print(len(faces))
     if key ==ord(" "):
         cv2.imwrite("frame-" + str(time.strftime("%H:%M:%S", time.localtime())) + ".jpg", frame)
         
@@ -69,4 +85,6 @@ while True:
     #print("frames per second are",int(fps)) ## if you increase the resolution of the image the fps will go down
     
 cv2.destroyAllWindows()
+
+
 
