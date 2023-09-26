74ccd229c4900a649c3b80d476c9c951bd0c0541
Jaden Ambrocio Alcantara <73153822+jaiecodes@users.noreply.github.com>
2023-09-26 16:07:52 -0500

    sadjkjasdksda

--- Lab_2/Part_1.py
+++ Lab_2/Part_1.py
@@ -5,3 +5,18 @@ import scapy
 
 capture = sniff()
 wrpcap("Part1.pcap", capture)
+
+f = open("Part1.pcap",'r')
+for row in f:
+    row = row.split(' ')
+    names.append(row[0])
+    marks.append(int(row[1]))
+  
+plt.bar(names, marks, color = 'g', label = 'File Data')
+  
+plt.xlabel('Student Names', fontsize = 12)
+plt.ylabel('Marks', fontsize = 12)
+  
+plt.title('Students Marks', fontsize = 20)
+plt.legend()
+plt.show()
--- Lab_2/testing.py
+++ Lab_2/testing.py
@@ -2,40 +2,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scapy.all import *
 
+def read_pcap(file):
+    time = []
+    motion = []
 
-
-def analyze_pcap(pcap_file):
-    # Initialize variables to store motion event data
-    timestamps = []
-    motion_events = []
-
-    # Open the pcap file and analyze packets
-    packets = rdpcap(pcap_file)
+    packets = rdpcap(file)
     for packet in packets:
-        # Implement your logic here to detect motion events.
-        # You might look for specific patterns or protocols related to motion detection.
-
-        # Example logic: Detect packets with a specific port number (adjust as needed)
         if packet.haslayer(UDP) and packet[UDP].dport == 5000:
-            timestamps.append(packet.time)
-            motion_events.append(1)
+            time.append(packet.time)
+            motion.append(1)
         else:
-            timestamps.append(packet.time)
-            motion_events.append(0)
+            time.append(packet.time)
+            motion.append(0)
 
-    return timestamps, motion_events
+    return time, motion
 
-def plot_timeline(timestamps, motion_events):
+def plot(time, motion):
     plt.figure(figsize=(12, 6))
-    plt.plot(timestamps, motion_events, 'r-', linewidth=1)
+    plt.plot(time, motion, 'r-', linewidth=1)
     plt.xlabel('Time')
-    plt.ylabel('Motion Detection')
+    plt.ylabel('Is Motion Dectected?')
     plt.title('Motion Detection Timeline')
-    plt.ylim([-0.1, 1.1])  # Set y-axis limits
+    plt.ylim([-0.1, 1.1])
     plt.grid(True)
     plt.show()
 
 if __name__ == '__main__':
-    pcap_file = '/home/pi/Desktop/Code/PostLabs/CS437_Post_Labs/Lab_2/packet_capture.pcap'  # Replace with your pcap file path
-    timestamps, motion_events = analyze_pcap(pcap_file)
-    plot_timeline(timestamps, motion_events)
+    file = '/home/pi/Desktop/Code/PostLabs/CS437_Post_Labs/Lab_2/packet_capture.pcap'
+    time, motion = read_pcap(file)
+    plot(time, motion)
