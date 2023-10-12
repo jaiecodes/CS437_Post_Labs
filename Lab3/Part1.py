35b6d3620945d0e2c806ad63df66baf18f9eda66
Sid <martianinja37@gmail.com>
2023-10-12 18:25:02 -0500

    indebnting

--- Lab3/Part1.py
+++ Lab3/Part1.py
@@ -21,34 +21,34 @@ def captured_packet_callback(pkt): #x-axis
         gyro = sense.get_gyroscope_raw()
         mag = sense.get_compass_raw()
     
-    x_axis = True
-    for event in sense.stick.get_events():
-        if event.action == 'pressed' and event.direction == 'right':
-            x_axis = True
-            break 
-        if event.action == 'pressed' and event.direction == 'down':
-            x_axis = False
-            break
+        x_axis = True
+        for event in sense.stick.get_events():
+            if event.action == 'pressed' and event.direction == 'right':
+                x_axis = True
+                break 
+            if event.action == 'pressed' and event.direction == 'down':
+                x_axis = False
+                break
 
-    
+        
 
-    if x_axis is True:
-        x = accel['x']
-        y = 0.0 # consider making this an array so that if we want to run the analysis it can access the 0
-        z = 0.0  
-    else:
-        x = 0.0 
-        y = accel['y'] # consider making this an array so that if we want to run the analysis it can access the 0
-        z = 0.0  
+        if x_axis is True:
+            x = accel['x']
+            y = 0.0 # consider making this an array so that if we want to run the analysis it can access the 0
+            z = 0.0  
+        else:
+            x = 0.0 
+            y = accel['y'] # consider making this an array so that if we want to run the analysis it can access the 0
+            z = 0.0  
 
-    timestamp = datetime.now().strftime("%H:%M:%S")
-    print("Value of x:" + x + " Value of Y:" + y)
-    entry = str(time.time())+","+timestamp+","+str(x)+","+str(y)+","+str(z)+","+str(pkt.dBm_AntSignal)+"\n"
+        timestamp = datetime.now().strftime("%H:%M:%S")
+        print("Value of x:" + x + " Value of Y:" + y)
+        entry = str(time.time())+","+timestamp+","+str(x)+","+str(y)+","+str(z)+","+str(pkt.dBm_AntSignal)+"\n"
 
-    with open(filename, "a") as f:
-        f.write(entry)
-    
-    time.sleep(1)
+        with open(filename, "a") as f:
+            f.write(entry)
+        
+        time.sleep(1)
 
 if __name__ == "__main__":
     sense=SenseHat()
