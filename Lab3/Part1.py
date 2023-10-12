676ce2f1b5653a369a6f44a982af12f15565a75b
Sid <martianinja37@gmail.com>
2023-10-12 17:04:25 -0500

    testing

--- Lab3/Part1.py
+++ Lab3/Part1.py
@@ -18,3 +18,29 @@ iface_n = "wlan1"  # Interface for network adapter
 
 timestamp_fname=datetime.now().strftime("%H:%M:%S")
 filename=path+timestamp_fname+".csv"
+
+def captured_packet_callback(pkt): #x-axis
+    if pkt.haslayer(Dot11) and pkt.addr2 == dev_mac:
+        accel = sense.get_accelerometer_raw()
+        gyro = sense.get_gyroscope_raw()
+        mag = sense.get_compass_raw()
+
+        x = accel['x']
+        y = 0 # consider making this an array so that if we want to run the analysis it can access the 0
+        z = 0  
+
+        timestamp = datetime.now().strftime("%H:%M:%S")
+
+        entry = str(time.time())+","+timestamp+","+str(x)+","+str(y)+","+str(z)+","+str(pkt.dBm_AntSignal)+"\n"
+
+        with open(filename, "a") as f:
+            f.write(entry)
+        
+        time.sleep(1)
+
+
+
+if __name__ == "__main__":
+    sense=SenseHat()
+    sense.set_imu_config(True,True,True) ## Config the Gyroscope, Accelerometer, Magnetometer
+    sniff(iface=iface_n, prn=captured_packet_callback)
