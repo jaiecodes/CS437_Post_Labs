cc4eb5b48bd22fe8a9d634c6e9866c3677fbf8fc
Jaden Ambrocio Alcantara <73153822+jaiecodes@users.noreply.github.com>
2023-10-12 17:14:29 -0500

    Update Part1.py

--- Lab3/Part1.py
+++ Lab3/Part1.py
@@ -19,15 +19,35 @@ iface_n = "wlan1"  # Interface for network adapter
 timestamp_fname=datetime.now().strftime("%H:%M:%S")
 filename=path+timestamp_fname+".csv"
 
-def captured_packet_callback(pkt): #x-axis
+def captured_x_packet_callback(pkt): #x-axis
     if pkt.haslayer(Dot11) and pkt.addr2 == dev_mac:
         accel = sense.get_accelerometer_raw()
         gyro = sense.get_gyroscope_raw()
         mag = sense.get_compass_raw()
 
         x = accel['x']
-        y = 0 # consider making this an array so that if we want to run the analysis it can access the 0
-        z = 0  
+        y = 0.0 # consider making this an array so that if we want to run the analysis it can access the 0
+        z = 0.0  
+
+        timestamp = datetime.now().strftime("%H:%M:%S")
+
+        entry = str(time.time())+","+timestamp+","+str(x)+","+str(y)+","+str(z)+","+str(pkt.dBm_AntSignal)+"\n"
+
+        with open(filename, "a") as f:
+            f.write(entry)
+        
+        time.sleep(1)
+
+
+def captured_y_packet_callback(pkt): #x-axis
+    if pkt.haslayer(Dot11) and pkt.addr2 == dev_mac:
+        accel = sense.get_accelerometer_raw()
+        gyro = sense.get_gyroscope_raw()
+        mag = sense.get_compass_raw()
+
+        x = 0.0 
+        y = accel['y'] # consider making this an array so that if we want to run the analysis it can access the 0
+        z = 0.0  
 
         timestamp = datetime.now().strftime("%H:%M:%S")
 
@@ -40,7 +60,19 @@ def captured_packet_callback(pkt): #x-axis
 
 
 
+
 if __name__ == "__main__":
     sense=SenseHat()
+    bool y_start = false
     sense.set_imu_config(True,True,True) ## Config the Gyroscope, Accelerometer, Magnetometer
-    sniff(iface=iface_n, prn=captured_packet_callback)
+    x = AsyncSniffer(iface=iface_n, prn=captured_x_packet_callback, store=0)
+    y = AsyncSniffer(iface=iface_n, prn=captured_y_packet_callback, store=0)
+    for event in sense.stick.get_events():
+        if event.action == 'pressed' and event.direction == 'right':
+            x.start()
+            if y_start is true
+                y.stop()
+        if event.action == 'pressed' and event.direction == 'down':
+            y_start = true
+            y.start()
+            x.stop()
