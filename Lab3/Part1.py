6252236b0eb365870748b680a24e3b1c66f2d941
Sid <martianinja37@gmail.com>
2023-10-12 16:55:58 -0500

    asf

--- Lab3/Part1.py
+++ Lab3/Part1.py
@@ -7,3 +7,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scipy
 from scapy.all import *
+
+import scipy.integrate as integrate
+path="/home/pi/Desktop/Code/Lab3/Part2/IMUDataRSSI"
+
+
+
+dev_mac = "e4:5f:01:d4:9d:ce"  # Assigned transmitter MAC
+iface_n = "wlan1"  # Interface for network adapter
+
+timestamp_fname=datetime.now().strftime("%H:%M:%S")
+filename=path+timestamp_fname+".csv"
