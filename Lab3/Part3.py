8b8866d5ae30821ec5cfbfe14ba136782ab50b44
Jaden Ambrocio Alcantara <73153822+jaiecodes@users.noreply.github.com>
2023-10-12 22:52:36 -0500

    Update Part3.py

--- Lab3/Part3.py
+++ Lab3/Part3.py
@@ -1,49 +1,37 @@
 from sense_hat import SenseHat
 import time
-from datetime import datetime,date
+from datetime import datetime, date
 from scapy.all import *
 
-
-path="/home/pi/Desktop/IMUData"
-
-
+path = "/home/pi/Desktop/IMUData"
 
 dev_mac = "e4:5f:01:d4:9d:ce"  # Assigned transmitter MAC
 iface_n = "wlan1"  # Interface for network adapter
 
 rssi_max = -100000
-colours = [[255,0,0], [0,255,0], [0,0,255], [255,255,0], [255,0,255], [0,255,255]]
+colours = [[255, 0, 0], [0, 255, 0], [0, 0, 255], [255, 255, 0], [255, 0, 255], [0, 255, 255]]
 
-def captured_packet_callback(pkt): #x-axis  
+def captured_packet_callback(pkt):
     global rssi_max
-    if pkt.haslayer(Dot11) and pkt.addr2 == dev_mac: 
-      print("RSSI: "+str(pkt.dBm_AntSignal)+"\n")     
-      print("MAXXX: "+str(rssi_max)+"\n")    
-      if abs(pkt.dBm_AntSignal) < abs(rssi_max):
-        print("GREEEN \n")    
-        rssi_max = pkt.dBm_AntSignal
-        sense.set_pixel(7,7, colours[1])
-      elif abs(pkt.dBm_AntSignal) < abs(rssi_max) - 3: 
-        print("TEAL "+str(abs(rssi_max) - 3)+"\n")  
-        sense.set_pixel(7,7, colours[5])
-      elif abs(pkt.dBm_AntSignal) < abs(rssi_max) - 5: 
-        print("BLUE "+str(abs(rssi_max) - 5)+"\n")  
-        sense.set_pixel(7,7, colours[2])
-      elif abs(pkt.dBm_AntSignal) < abs(rssi_max) - 10: 
-        print("RED "+str(abs(rssi_max) - 10)+"\n")  
-        sense.set_pixel(7,7, colours[0])
-      time.sleep(0.1)
+    if pkt.haslayer(Dot11) and pkt.addr2 == dev_mac:
+        print("RSSI: " + str(pkt.dBm_AntSignal) + "\n")
+        print("MAXXX: " + str(rssi_max) + "\n")
+        if pkt.dBm_AntSignal > rssi_max:  # Check if the new RSSI is greater than the current maximum
+            rssi_max = pkt.dBm_AntSignal
+            set_pixel_color(rssi_max)
+        time.sleep(0.1)
 
+def set_pixel_color(rssi_value):
+    if rssi_value >= rssi_max:  # Set the pixel color based on the maximum RSSI value
+        sense.set_pixel(7, 7, colours[1])
+    elif rssi_max - 3 <= rssi_value < rssi_max:
+        sense.set_pixel(7, 7, colours[5])
+    elif rssi_max - 5 <= rssi_value < rssi_max - 3:
+        sense.set_pixel(7, 7, colours[2])
+    elif rssi_max - 10 <= rssi_value < rssi_max - 5:
+        sense.set_pixel(7, 7, colours[0])
 
-if __name__ == "__main__":
-    sense=SenseHat()
+if __name__ == "__main":
+    sense = SenseHat()
     sense.clear()
     sniff(iface=iface_n, prn=captured_packet_callback)
-    
-    
-    
-
-
-       
-
-
