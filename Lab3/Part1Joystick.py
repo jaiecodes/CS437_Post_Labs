6edc7ea03c8ab95b00053defcf48ab1dbb514a47
Sid <martianinja37@gmail.com>
2023-10-12 21:07:59 -0500

    Update Part1Joystick.py

--- Lab3/Part1Joystick.py
+++ Lab3/Part1Joystick.py
@@ -17,10 +17,22 @@ height=1.95 # in meters
 step_length= 0.415 * height # in meters
 xpos = 0.0
 ypos = 0.0
+init = False
 def captured_packet_callback(pkt): #x-axis  
     global xpos
     global ypos
     if pkt.haslayer(Dot11) and pkt.addr2 == dev_mac:
+        
+        
+        if init is False:
+            timestamp = datetime.now().strftime("%H:%M:%S")
+        
+            entry = str(time.time())+","+timestamp+","+str(xpos)+","+str(ypos)+","+str(0.0)+","+str(0)+"\n"
+
+            with open(filename, "a") as f:
+                f.write(entry)
+            init = True
+        
         accel = sense.get_accelerometer_raw()
         gyro = sense.get_gyroscope_raw()
         mag = sense.get_compass_raw()
@@ -51,13 +63,7 @@ if __name__ == "__main__":
     
     sense=SenseHat()
     sense.set_imu_config(True,True,True) ## Config the Gyroscope, Accelerometer, Magnetometer
-    timestamp = datetime.now().strftime("%H:%M:%S")
-    #print("Value of x:" + x + " Value of Y:" + y)
-    entry = str(time.time())+","+timestamp+","+str(xpos)+","+str(ypos)+","+str(0.0)+","+str(pkt.dBm_AntSignal)+"\n"
-
-    with open(filename, "a") as f:
-        f.write(entry)
-        
+    
         
     sniff(iface=iface_n, prn=captured_packet_callback)
     
