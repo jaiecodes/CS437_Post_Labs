781447d36b35d047d8ae134ceb1d447a6a6c11e4
Sid <martianinja37@gmail.com>
2023-10-12 19:49:11 -0500

    Update Analysis.py

--- Lab3/Analysis.py
+++ Lab3/Analysis.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 
 
-filename="/home/pi/Desktop/IMUData19:38:04.csv"
+filename="/home/pi/Desktop/IMUData19:46:44.csv"
 
 ## CSV file template:
 # time in seconds, timestamp (H:M:S), X-Acceleration, Y-Acceleration, Z-Acceleration, X-Gyroscope, Y-Gyro,Z-Gyro, X-Gyro, Y-Gyro, Z-gyro
