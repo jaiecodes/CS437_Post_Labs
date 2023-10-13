f66bf3b472743111bb123a3e1e4b4817555632cc
Jaden Ambrocio Alcantara <73153822+jaiecodes@users.noreply.github.com>
2023-10-12 23:06:47 -0500

    Update Part3.py

--- Lab3/Part3.py
+++ Lab3/Part3.py
@@ -19,17 +19,17 @@ def captured_packet_callback(pkt): #x-axis
     if pkt.haslayer(Dot11) and pkt.addr2 == dev_mac: 
       print("RSSI: "+str(pkt.dBm_AntSignal)+"\n")     
       print("MAXXX: "+str(rssi_max)+"\n")    
-      if abs(pkt.dBm_AntSignal) < abs(rssi_max):
+      if abs(pkt.dBm_AntSignal) > abs(rssi_max):
         print("GREEEN \n")    
         rssi_max = pkt.dBm_AntSignal
         sense.set_pixel(7,7, colours[1])
-      elif abs(pkt.dBm_AntSignal) < abs(rssi_max) - 3: 
+      elif abs(pkt.dBm_AntSignal) > abs(rssi_max) + 3: 
         print("TEAL "+str(abs(rssi_max) - 3)+"\n")  
         sense.set_pixel(7,7, colours[5])
-      elif abs(pkt.dBm_AntSignal) < abs(rssi_max) - 5: 
+      elif abs(pkt.dBm_AntSignal) > abs(rssi_max) + 5: 
         print("BLUE "+str(abs(rssi_max) - 5)+"\n")  
         sense.set_pixel(7,7, colours[2])
-      elif abs(pkt.dBm_AntSignal) < abs(rssi_max) - 10: 
+      elif abs(pkt.dBm_AntSignal) > abs(rssi_max) + 10: 
         print("RED "+str(abs(rssi_max) - 10)+"\n")  
         sense.set_pixel(7,7, colours[0])
       time.sleep(0.1)
