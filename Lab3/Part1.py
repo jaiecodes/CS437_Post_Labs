1b375bf3e30adb43dc04e22506297aae8546bdd5
Sid <martianinja37@gmail.com>
2023-10-12 19:26:19 -0500

    Update Part1.py

--- Lab3/Part1.py
+++ Lab3/Part1.py
@@ -15,7 +15,7 @@ iface_n = "wlan1"  # Interface for network adapter
 timestamp_fname=datetime.now().strftime("%H:%M:%S")
 filename=path+timestamp_fname+".csv"
 
-global initialized
+
 def captured_packet_callback(pkt): #x-axis
     global x_pos 
     global y_pos
@@ -39,7 +39,7 @@ def captured_packet_callback(pkt): #x-axis
             if event.action == 'pressed' and event.direction == 'left':
                 x_pos = 0.0
                 y_pos = 0.0
-                initialized = True
+                break
             if event.action == 'release' and event.direction == 'down':
                 x_pos =  accel['x']
                 break
@@ -48,32 +48,32 @@ def captured_packet_callback(pkt): #x-axis
                 break
             
 
-        if initialized is True:
-            if disabled is True:
-                x = x_pos
+        
+        if disabled is True:
+            x = x_pos
+            y = y_pos # consider making this an array so that if we want to run the analysis it can access the 0
+            z = 0.0  
+        else:
+            if x_axis is True:
+                x = accel['x']
                 y = y_pos # consider making this an array so that if we want to run the analysis it can access the 0
                 z = 0.0  
             else:
-                if x_axis is True:
-                    x = accel['x']
-                    y = y_pos # consider making this an array so that if we want to run the analysis it can access the 0
-                    z = 0.0  
-                else:
-                    x = x_pos 
-                    y = accel['y'] # consider making this an array so that if we want to run the analysis it can access the 0
-                    z = 0.0  
+                x = x_pos 
+                y = accel['y'] # consider making this an array so that if we want to run the analysis it can access the 0
+                z = 0.0  
 
-            timestamp = datetime.now().strftime("%H:%M:%S")
-            #print("Value of x:" + x + " Value of Y:" + y)
-            entry = str(time.time())+","+timestamp+","+str(x)+","+str(y)+","+str(z)+","+str(pkt.dBm_AntSignal)+"\n"
+        timestamp = datetime.now().strftime("%H:%M:%S")
+        #print("Value of x:" + x + " Value of Y:" + y)
+        entry = str(time.time())+","+timestamp+","+str(x)+","+str(y)+","+str(z)+","+str(pkt.dBm_AntSignal)+"\n"
 
-            with open(filename, "a") as f:
-                f.write(entry)
-            
-            #time.sleep(1)
+        with open(filename, "a") as f:
+            f.write(entry)
+        
+        #time.sleep(1)
 
 if __name__ == "__main__":
-    initialized = False
+    
     sense=SenseHat()
     sense.set_imu_config(True,True,True) ## Config the Gyroscope, Accelerometer, Magnetometer
     sniff(iface=iface_n, prn=captured_packet_callback)
