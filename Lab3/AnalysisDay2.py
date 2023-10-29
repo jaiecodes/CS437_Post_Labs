1ba8dcca514cced0f1a10801a6bf86678efa318c
Jaden Ambrocio Alcantara <73153822+jaiecodes@users.noreply.github.com>
2023-10-28 22:31:01 -0500

    Update AnalysisDay2.py

--- Lab3/AnalysisDay2.py
+++ Lab3/AnalysisDay2.py
@@ -148,7 +148,7 @@ for i in range(len(count)):
 
 avg_RSSIs[0] = [-53, 1]
 rssi_value = np.zeros(max_count + 2)
-print(avg_RSSIs[count[i]])
+print(str(avg_RSSIs[0]))
 for step_num, rssi_list in avg_RSSIs.items():
   rssi_value[step_num] = rssi_list[0] / rssi_list[1]
   print("Step Count: "+ str(step_num))
