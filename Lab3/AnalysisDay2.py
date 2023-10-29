ebf3d10ed9af24e024177a690f11dc75eb6d8fda
Jaden Ambrocio Alcantara <73153822+jaiecodes@users.noreply.github.com>
2023-10-28 21:50:45 -0500

    Update AnalysisDay2.py

--- Lab3/AnalysisDay2.py
+++ Lab3/AnalysisDay2.py
@@ -144,7 +144,7 @@ for i in range(len(count)):
 
 
 rssi_value = np.zeros(max_count + 2)
-rssi_value[0] = 0
+rssi_value[0] = -53
 for step_num, rssi_list in avg_RSSIs.items():
   if step_num == 0:
     continue
